fdb48d6b9a22e0f917eaa1b0d28d595a1ad5fed9
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2020-02-10 18:52:17 +0100

    added test of the show() function

--- main.py
+++ main.py
@@ -10,11 +10,22 @@ def check():
 
 
 def show(matrix, colors={}):
-    matrix = [colors[s] for s in matrix]]
+    matrix = [colors[s] for s in matrix]
     sense.set_pixels = matrix
 
 if __name__ == "__main__":
         check()
+        c = {0: [0, 0, 0], 1: [0, 255, 0]}
+        m = [O, O, O, 1, 1, O, O, O,
+            O, O, 1, O, O, 1, O, O,
+            O, O, O, O, O, 1, O, O,
+            O, O, O, O, 1, O, O, O,
+            O, O, O, 1, O, O, O, O,
+            O, O, O, 1, O, O, O, O,
+            O, O, O, O, O, O, O, O,
+            O, O, O, 1, O, O, O, O]
+
+        show(m)
 
 
 sense = SenseHat()

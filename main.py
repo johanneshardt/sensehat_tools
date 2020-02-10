d635f743bb59670e697d6e5716ab0660ca6881c8
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2020-02-10 18:46:55 +0100

    added show()

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from sense_hat import SenseHat
 
+sense = SenseHat()
+
 def check():
-        sense = SenseHat()
         print("Humidity:     {}".format(sense.humidity))
         print("Temperatur:   {}".format(sense.temp))
         print("Pressure:     {}".format(sense.pressure))
         print("North:        {}".format(sense.compass))
+
+
+def show(matrix, colors={}):
+    matrix = [colors[s] for s in matrix]]
+    sense.set_pixels = matrix
 
 if __name__ == "__main__":
         check()

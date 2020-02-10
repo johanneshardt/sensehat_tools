2052b45cb0791919c1fa5bb51b69e897bdca2ee9
johanneshardt <46093733+johanneshardt@users.noreply.github.com>
2020-02-10 20:01:44 +0100

    First iteration of snake game

--- main.py
+++ main.py
@@ -13,16 +13,51 @@ def show(matrix, colors={}):
     matrix = [colors[s] for s in matrix]
     sense.set_pixels(matrix)
 
+
+def s_game():
+    class Snek():
+        def __init__(self):
+            self.status = True
+            self.length = 2
+            self.color = [0, 255, 0]
+            self.background = [0, 0, 0]
+            self.position = (3, 0)
+            self.direction = 'r'
+        
+
+        def __repr__(self):
+            return "snek -> status={},  length={}".format(self.status, self.length)
+    
+
+        def move(input=self.direction, pos=self.position):
+            directions = {'u': lambda pos: (pos[0]-1, pos[1]),
+                          'l': lambda pos: (pos[0], pos[1]-1), 
+                          'd': lambda pos: (pos[0]+1, pos[1]), 
+                          'r': lambda pos: (pos[0], pos[1]+1)}
+
+            self.position = directions[input](pos)
+
+
+        def draw(pos=self.position):
+            color = {0: self.background, 1: self.color}
+            matrix = [0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0]
+            
+            matrix[(pos[0]-1)+pos[1]] = 1
+            show(matrix, color)
+
+        def collision(self):
+            self.status = False
+ 
+    s = Snek()
+    s.draw()
+
 if __name__ == "__main__":
         check()
-        c = {0: [0, 0, 0], 1: [0, 255, 0]}
-        m = [0, 0, 0, 1, 1, 0, 0, 0,
-            0, 0, 1, 0, 0, 1, 0, 0,
-            0, 0, 0, 0, 0, 1, 0, 0,
-            0, 0, 0, 0, 1, 0, 0, 0,
-            0, 0, 0, 1, 0, 0, 0, 0,
-            0, 0, 0, 1, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 1, 0, 0, 0, 0]
-
-        show(m, colors=c)
+        s_game()

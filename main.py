3ab206fd4b395c3a4e19c46ce73779938c5f6cea
Johannes Hardt <lunahardt1@outlook.com>
2020-02-12 10:14:24 +0000

    restructiuring and fixes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sense_hat import SenseHat, ACTION_PRESSED
 from time import sleep
 
 sense = SenseHat()
+sense.flip_v()
 
 def check():
         print("Humidity:     {}".format(sense.humidity))
@@ -10,61 +11,71 @@ def check():
         print("North:        {}".format(sense.compass))
 
 
-def show(matrix, colors={}):
+def show(matrix, colors={}, brightness=1):
+    for color in colors:
+        for value in color:
+            value *= brightness
+
     matrix = [colors[s] for s in matrix]
     sense.set_pixels(matrix)
 
+
+class Matrix():
+    def __init__(self):
+        self.matrix()
 
 def s_game():
     class Snek():
         def __init__(self):
             self.status = True
-            self.length = 2
+            self.length = 1
             self.color = [0, 255, 0]
             self.background = [0, 0, 0]
-            self.position = (3, 0)
-            self.direction = 'r'
-            self.moves = ['u', 'd']
+            self.position = (0,3)       # position as in a cartesian coordinate system
+            self.direction = 2   
+            self.moved = False    
+            self.moves = [1, -1]
             self.speed = 0.7
             self.bounds = [[0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0]]
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0]]
         
 
         def __repr__(self):
             return "snek -> status={},  length={}".format(self.status, self.length)
     
 
-        def move(self, input=None):
-            if input is None:
-                input = self.direction
-            
+        def move(self, input=self.direction):
             pos = self.position
-            directions = {'u': lambda pos: (pos[0]-1, pos[1]),
-                          'l': lambda pos: (pos[0], pos[1]-1), 
-                          'd': lambda pos: (pos[0]+1, pos[1]), 
-                          'r': lambda pos: (pos[0], pos[1]+1)}
 
-            new_pos = directions[input](pos)
+            directions = { 1: lambda pos: (pos[0], pos[1]+1), #up
+                          -1: lambda pos: (pos[0], pos[1]-1), #down
+                           2: lambda pos: (pos[0]+1, pos[1]), #right
+                          -2: lambda pos: (pos[0]-1, pos[1])} #left
+
+            x, y = directions[input](pos)
 
             try:
-                self.bounds[new_pos[0]][new_pos[1]]
-                self.position = new_pos
+                self.bounds[x][y]
 
             except IndexError:
                 self.status = False
+                return
+            
+            self.position = (x,y)
+            self.moved + True
 
 
         def draw(self, pos=None):
             if pos is None:
                 pos = self.position
             
-            color = {0: self.background, 1: self.color}
+            color = {0: self.background, 1: self.color} # 9-3
             matrix = [0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
@@ -74,33 +85,34 @@ def s_game():
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0]
             
-            matrix[((pos[0])*8)+pos[1]] = 1
-            show(matrix, color)
+            matrix[(pos[0]*8)+pos[1]] = 1
+            show(matrix, color, brightness=0.5)
 
 
-        def set_direction(self, event):
-            input = self.direction
-            opposite = {'u': 'd', 'l': 'r', 'd': 'u', 'r': 'l'}
-            self.moves = [move for move in ['u', 'l', 'd', 'r'] if move not in [input, opposite[input]]]
+        def set_direction(self, event, input=self.direction):
+            self.moves = [move for move in [1,-1,2,-2] if move not in [input, -input]]
             
-            convert = {'up': 'u', 'left': 'l', 'down': 'd', 'right': 'r', 'middle': 'r'}
+            convert = {'up': 1, 'left': -2, 'down': -1, 'right': 2, 'middle': 1}
             if event.action == ACTION_PRESSED:
                 new_d = convert[event.direction]
-                if new_d in self.moves:
+                if new_d in self.moves and self.moved:
                     self.direction = new_d
+                    self.moved = False
 
 
-        def death(self):
-            blank = [0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0,
-                     0, 0, 0, 0, 0, 0, 0, 0]
+        def death(self, pos=self.position, steps=50): # steps param used for testing
+            red = {0: [255,0,0]}
 
-            show(blank, {0: [255, 0, 0]})
+            screen = [0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0]
+                      
+            show(screen, {0: [255, 0, 0]})
             sleep(0.1)
             show(blank, {0: [0, 0, 0]})
             sense.show_message('Score: {}'.format(self.length))
@@ -124,6 +136,8 @@ def s_game():
                 self.position = (3, 0)
     s = Snek()
     s.main()
+
+
 if __name__ == "__main__":
         check()
         s_game()

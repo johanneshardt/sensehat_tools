8bfc546a3779f1e125814e6ccdd88d13e95691f3
Johannes Hardt <lunahardt1@outlook.com>
2020-02-14 10:39:18 +0000

    fix

--- main.py
+++ main.py
@@ -44,36 +44,28 @@ def s_game():
     
 
         def move(self, input=None):
-
             if input is None:
                 input = self.direction
 
             pos = self.position
-
             directions = { 1: lambda pos: (pos[0], pos[1]-1), #up
                           -1: lambda pos: (pos[0], pos[1]+1), #down
                            2: lambda pos: (pos[0]+1, pos[1]), #right
                           -2: lambda pos: (pos[0]-1, pos[1])} #left
-
             x, y = directions[input](pos)
-
             if x < 0 or x > 7 or y < 0 or y > 7:
                 self.status = False
             else:
                 self.position = (x,y)
-
                 if self.trail[0] == self.fruit:
                     self.lenth += 1
                     self.trail = deque(self.trail, maxlen=self.length)
-
                 self.trail.append(self.position)
                 self.moved = True
 
 
         def draw(self):
-
             color = {0: self.b_color, 1: self.s_color, 2: self.f_color}
-
             matrix = [0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
@@ -82,48 +74,35 @@ def s_game():
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0]
-            
             for part in self.trail:
                 print(part)
                 matrix[part[0]+part[1]*8] = 1
-            
             matrix[self.fruit[0]+self.fruit[1]*8] = 2
-
             show(matrix, color, brightness=0.5)
 
 
         def set_direction(self, event, input=None):
-
             if input is None:
                 input = self.direction
-
             self.moves = [move for move in [1,-1,2,-2] if move not in [input, -input]]
-            
             convert = {'up': 1, 'left': -2, 'down': -1, 'right': 2, 'middle': 1}
-
             if event.action == ACTION_PRESSED:
                 new_d = convert[event.direction]
-
                 if new_d in self.moves:
-
                     if self.moved:
                         self.direction = new_d
                         self.moved = False
 
+
         def spawn(self):
             possible = [spot for spot in self.matrix if spot not in self.trail]
             self.fruit = choice(possible)
-
-
             
             
         def death(self, pos=None, steps=50): # steps param used for testing
-
             if pos is None:
                 pos = self.position
-                
             red = {0: [255,0,0]}
-
             screen = [0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
@@ -132,7 +111,6 @@ def s_game():
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0,
                       0, 0, 0, 0, 0, 0, 0, 0]
-
             show(screen, {0: [255, 0, 0]})
             sleep(0.1)
             show(screen, {0: [0, 0, 0]})
@@ -145,12 +123,10 @@ def s_game():
                 print('New game')
                 self.spawn()
                 self.draw()
-
                 while self.status:
                     sleep(self.speed)
                     self.move()
                     self.draw()     
-
                 self.death()
                 sense.stick.wait_for_event(emptybuffer=True)
                 self.status = True

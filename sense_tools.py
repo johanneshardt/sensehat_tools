e3c8f531f2cdc3ae77a4784f3b79a0a5d5da448e
Johannes Hardt <lunahardt1@outlook.com>
2020-02-17 21:34:25 +0000

    restructuring, using black formatting

--- sense_tools.py
+++ sense_tools.py
@@ -3,22 +3,23 @@ from sense_hat import SenseHat
 
 
 sense = SenseHat()
-parser = argparse.ArgumentParser(prog='sense_tools',
-                                 description='''
-                                        A collection of projects demonstrating the capabilities of 
-                                        a raspberry pi sensehat-addon.''',
-                                        
-                                epilog='Created by Johannes hardt, 2020.')
+parser = argparse.ArgumentParser(
+    prog="sense_tools",
+    description="A collection of projects demonstrating the capabilities of a raspberry pi sensehat-addon.",
+    epilog="Created by Johannes hardt, 2020.",
+)
+
 
 def check():
-        print("Humidity:     {}".format(sense.humidity))
-        print("Temperatur:   {}".format(sense.temp))
-        print("Pressure:     {}".format(sense.pressure))
-        print("North:        {}".format(sense.compass))
+    print("Humidity:     {}".format(sense.humidity))
+    print("Temperatur:   {}".format(sense.temp))
+    print("Pressure:     {}".format(sense.pressure))
+    print("North:        {}".format(sense.compass))
 
 
 if __name__ == "__main__":
-        check()
-        from snake import Snek
-        s = Snek()
-        s.main(speed=0.3)
+    check()
+    from snake import Snek
+
+    s = Snek()
+    s.main(speed=0.3)
--- snake.py
+++ snake.py
@@ -5,6 +5,7 @@ from random import choice
 
 sense = SenseHat()
 
+
 def show(matrix, colors={}, brightness=1):
     for color in colors.values():
         for value in color:
@@ -14,84 +15,87 @@ def show(matrix, colors={}, brightness=1):
     sense.set_pixels(matrix)
 
 
-class Snek():
+class Snek:
     def __init__(self):
-        self.b_color    = [0, 0, 0]
-        self.f_color    = [255, 0, 0]
-        self.s_color    = [0, 255, 0]
-        self.fo_color   = [255, 98, 0]
-        self.direction  = 2
-        self.fruit      = None          #this is set in the main() loop
-        self.old_fruit  = None
-        self.length     = 2
-        self.matrix     = [(x,y) for x in range(0,8) for y in range (0,8)]
-        self.moved      = True   
-        self.moves      = [1, -1]
-        self.position   = (2, 3)
-        self.speed      = 0.5
-        self.status     = True
-        self.trail      = deque([(1, 3), (2, 3)], maxlen=self.length)
-    
+        self.colors = {
+            "background": [0, 0, 0],
+            "fruit": [255, 0, 0],
+            "snake": [0, 255, 0],
+            "eaten_fruit": [255, 98, 0],
+        }
+        self.direction = 2
+        self.dimensions = (8, 8)
+        self.fruit = None  # this is set in the main() loop
+        self.eaten = None
+        self.length = 2
+        self.matrix = [(x, y) for x in range(0, 8) for y in range(0, 8)]
+        self.moved = True
+        self.moves = [1, -1]
+        self.position = (2, 3)
+        self.speed = 0.5
+        self.status = True
+        self.trail = deque([(1, 3), (2, 3)], maxlen=self.length)
 
     def __repr__(self):
         return "snek -> status={},  length={}".format(self.status, self.length)
-
 
     def move(self, input=None):
         if input is None:
             input = self.direction
 
         pos = self.position
-        directions = { 1: lambda pos: (pos[0], pos[1]-1), #up
-                        -1: lambda pos: (pos[0], pos[1]+1), #down
-                        2: lambda pos: (pos[0]+1, pos[1]), #right
-                        -2: lambda pos: (pos[0]-1, pos[1])} #left
+        directions = {
+            1: lambda pos: (pos[0], pos[1] - 1),  # up
+            -1: lambda pos: (pos[0], pos[1] + 1),  # down
+            2: lambda pos: (pos[0] + 1, pos[1]),  # right
+            -2: lambda pos: (pos[0] - 1, pos[1]),
+        }  # left
         x, y = directions[input](pos)
         if x < 0 or x > 7 or y < 0 or y > 7:
-                self.status = False
+            self.status = False
         elif (x, y) in self.trail:
             self.status = False
         else:
-            self.position = (x,y)
+            self.position = (x, y)
             if self.trail[-1] == self.fruit:
-                self.old_fruit = self.fruit
+                self.eaten = self.fruit
                 self.spawn()
 
-            if self.trail[0] == self.old_fruit:
-                self.old_fruit = None
+            if self.trail[0] == self.eaten:
+                self.eaten = None
                 self.length += 1
                 self.trail = deque(self.trail, maxlen=self.length)
             self.trail.append(self.position)
             self.moved = True
 
-
     def draw(self):
-        color = {0: self.b_color, 1: self.f_color, 2: self.fo_color, 3: self.s_color}
-        matrix = [0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0]
-        b_step = [int((c-(0.8*c))/self.length) for c in self.s_color]
+        color = {
+            0: self.colors["background"],
+            1: self.colors["fruit"],
+            2: self.colors["eaten_fruit"],
+            3: self.colors["snake"],
+        }
+
+        matrix = [0 for i in range(self.dimensions[0] * self.dimensions[1])]
+        b_step = [int((c - (0.8 * c)) / self.length) for c in self.colors["snake"]]
 
         for index, part in enumerate(self.trail, 3):
-            color[index] = [c-(b_step[i]*(index-3)) for i, c in enumerate(self.s_color)]
+            color[index] = [
+                c - (b_step[i] * (index - 3))
+                for i, c in enumerate(self.colors["snake"])
+            ]
             print(str(color[index]))
-            matrix[part[0]+part[1]*8] = index
-        matrix[self.fruit[0]+self.fruit[1]*8] = 1
-        if self.old_fruit is not None:
-            matrix[self.old_fruit[0]+self.old_fruit[1]*8] = 2
+            matrix[part[0] + part[1] * 8] = index
+        matrix[self.fruit[0] + self.fruit[1] * 8] = 1
+        if self.eaten is not None:
+            matrix[self.eaten[0] + self.eaten[1] * 8] = 2
         show(matrix, color)
-
 
     def set_direction(self, event, input=None):
         if input is None:
             input = self.direction
-        self.moves = [move for move in [1,-1,2,-2] if move not in [input, -input]]
-        convert = {'up': 1, 'left': -2, 'down': -1, 'right': 2, 'middle': 1}
+        self.moves = [move for move in [1, -1, 2, -2] if move not in [input, -input]]
+        convert = {"up": 1, "left": -2, "down": -1, "right": 2, "middle": 1}
         if event.action == ACTION_PRESSED:
             new_d = convert[event.direction]
             if new_d in self.moves:
@@ -99,50 +103,40 @@ class Snek():
                     self.direction = new_d
                     self.moved = False
 
-
     def spawn(self):
         possible = [spot for spot in self.matrix if spot not in self.trail]
         self.fruit = choice(possible)
-        
-        
-    def death(self, pos=None, steps=50): # steps param used for testing
+
+    def death(self, pos=None, steps=50):  # steps param used for testing
         if pos is None:
             pos = self.position
-        color = {0: [0, 0, 0], 1: [255,0,0]}
-        screen = [0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0]
+        color = {0: [0, 0, 0], 1: [255, 0, 0]}
+        screen = [0 for i in range(self.dimensions[0] * self.dimensions[1])]
 
         for part in self.trail:
-            screen[part[0]+part[1]*8] = 1
+            screen[part[0] + part[1] * 8] = 1
             show(screen, color)
             sleep(0.03)
 
         sleep(0.5)
-        sense.show_message('Score: {}'.format(self.length), scroll_speed=0.05)
+        sense.show_message("Score: {}".format(self.length), scroll_speed=0.05)
 
-    
     def main(self, speed=0.5):
         self.speed = speed
         while True:
             sense.stick.direction_any = self.set_direction
-            print('New game')
+            print("New game")
             self.spawn()
             self.draw()
             while self.status:
                 sleep(self.speed)
                 self.move()
-                self.draw()     
+                self.draw()
             self.death()
             sense.stick.wait_for_event(emptybuffer=True)
             self.status = True
-            self.old_fruit = None
+            self.eaten = None
             self.length = 2
             self.direction = 2
-            self.position = (2,3)
+            self.position = (2, 3)
             self.trail = deque([(1, 3), (2, 3)], maxlen=self.length)
